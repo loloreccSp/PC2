695eebd9ed499012108ccb17c630823d2fc3dce8
Daniil <nclient78@gmail.com>
2023-08-03 19:01:58 +0300

    Created nested classes

---
+++ HelloWorld(Pro)/Musical_instruments.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Musical_instruments
+    {
+        public string name { get; set; }
+
+        public class Violin : Musical_instruments
+        {
+            
+        }
+
+        public class Trombone : Musical_instruments
+        {
+            
+        }
+
+        public class Ukulele : Musical_instruments
+        {
+            
+        }
+
+        public class Cello : Musical_instruments 
+        {
+
+        }
+
+    }
+}
